56eae835972f0fd304e6aee4477b42724578f092
rrenzo <145371176+rrenzo@users.noreply.github.com>
2023-11-04 23:09:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div class="stage">
+    <div class="actor"></div>
+    <div class="actor"></div>
+    <div class="actor"></div>
+</div>
+<div id="header">
+<h1>Raphael Renzo Martinez</h1>
+<h2>UPenn '27</h2>
+</div>
+<img src="/IMG-6921.jpg" alt="me" width="350" height="350"></img>
+<div id="about-me">   
+<p>Hey, I'm a freshman at the University of Pennsylvania planning to double major in Philosophy and Physics with a minor in Data Science and Analytics. I'm also into: </p>
+<ul>
+    <li>Sports</li>
+    <li>Filmmaking</li>
+    <li>Reading and Writing</li>
+    <li>Nature</li>
+</ul>
+</div>    
+<a href='https://github.com/rrenzo'>GitHub</a>
+</body>
+  );
+}
+
+export default App;
